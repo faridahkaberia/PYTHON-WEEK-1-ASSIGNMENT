9b4ad3409408731df322eff360dcc494e0dbe831
faridah kaberia <faridahkaberia@gmail.com>
2024-08-25 12:31:27 +0300

    Here is my code

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# Ask the user for their name, age and location
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+# Print a personalised mesaage.
+print(f"Hello {name}! You are {age} years old and live in {location}.")
+
+
